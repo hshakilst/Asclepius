d4d7c60d1bf822f55fd73b975e4914e5ef3deb07
Shakil Hossain <hshakilst@gmail.com>
2019-05-02 09:25:12 +0600

    Changed data source from medex.com.bd to dgda.gov.bd

--- firebase_cloud_function/functions/index.js
+++ firebase_cloud_function/functions/index.js
@@ -2,28 +2,39 @@ const functions = require("firebase-functions");
 const request = require("request");
 
 exports.search = functions.https.onRequest((req, res) => {
-  if (req.query.keyword && req.query.keyword !== '' && req.method === 'POST') {
+  if (req.query.keyword && req.query.keyword !== "" && req.method === "POST") {
+    var url =
+      "https://dgda.gov.bd/administrator/components/com_jcode/source/serverProcessing.php";
+
+    //set header
+    var headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    };
+
+    //set form data
+    var form = {
+      action: "getDrugCompanyDatabaseSearchPriceData",
+      FilterAll: 4,
+      FilterItem: req.query.keyword
+    };
+
+    //set request parameter
+
     request.post(
-      "https://wrapapi.com/use/hshakilst/drugs/medex/0.0.3",
-      {
-        json: {
-          keyword: req.query.keyword,
-          wrapAPIKey: "VZkDFOxJfYaNytuvlkHnx4zpcXuGAQIF"
-        }
-      },
+      { headers: headers, url: url, form: form, method: "POST" },
       (error, response, body) => {
-        if (!error && parseInt(response.statusCode) === 200 && body.data) {
-          res.status(200).json(body.data["output"]);
+        if (!error && parseInt(response.statusCode) === 200 && body) {
+          res.status(200).send(body);
         } else {
-          res.status(404).json({"msg": "Search results not found!"});
+          res.status(404).json({ msg: "Search results not found!" });
         }
       }
     );
-  }
-  else if(!(req.query.keyword && req.query.keyword !== '' && req.method === 'POST')){
-      res.status(400).json({"msg": "Bad Request!"});
-  }
-  else{
-      res.status(500).json({"msg": "Internal server error!"});
+  } else if (
+    !(req.query.keyword && req.query.keyword !== "" && req.method === "POST")
+  ) {
+    res.status(400).json({ msg: "Bad Request!" });
+  } else {
+    res.status(500).json({ msg: "Internal server error!" });
   }
 });
